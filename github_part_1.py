fb8061d1754403b6c623cfba6e408fa8a835f332
Arturo9561 <37811770+Arturo9561@users.noreply.github.com>
2021-01-26 11:47:48 +0100

    First Upload: Visualization of Finance Index Values of Companies
    Part 1: Preparation
    Part 2: Visualization

---
+++ github_part_1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 17:09:42 2021
+
+@author: Nurcan Düzenli
+"""
+import pandas as pd
+import datetime as dt
+
+import bs4
+import requests
+from bs4 import BeautifulSoup
+
+def real_time_price(symbol):
+    url = ('https://finance.yahoo.com/quote/') + symbol + ('/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAINGLolHcuHio-RL9CxZfo-oVtwUkHS8YcK0YIVrQ5xq_lIZ-fIAs3-ES7u3LEWmVJzbO_TWMRxthn-ztc1PdHz_fZpWossP2BfNkD36qNOb5bVSIYCNi9do9p3gNPWEDkG-EcjPZfyVt4ByNTINX2nqoTKFGg8tvjPk8dKWJRxS')
+    r = requests.get(url)
+    web_content = BeautifulSoup(r.text, 'lxml')
+    web_content = web_content.find('div', {"class":'My(6px) Pos(r) smartphone_Mt(6px)'})
+    web_content = web_content.find('span').text
+
+    return web_content
+
+NASDAQ = ['TSLA', 'AAPL', 'BNTX', 'KO']
+web_content = real_time_price('TSLA')
+
+for step in range(1,101):   
+    
+    price = []
+    col = []
+    time_stamp = dt.datetime.now()
+    time_stamp = time_stamp.strftime("%Y-%m-%d %H:%M:%S")
+    
+    for symbol in NASDAQ:
+        price.append(real_time_price(symbol))
+        
+    col = [time_stamp]
+    col.extend(price)
+    
+    df = pd.DataFrame(col)
+    
+    df = df.T
+    
+    df.to_csv('real time stock data.csv', mode = 'a', header = False)
+    print(col)
+    
+    
---
+++ github_part_2.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 22 19:28:54 2021
+
+@author: Nurcan Düzenli
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+
+
+style.use('fivethirtyeight')
+style.use('dark_background')
+fig = plt.figure()
+
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
+
+def animate(i):
+    df = pd.read_csv('real time stock data.csv')
+    ys = df.iloc[1:,2].values
+    xs = list(range(1, len(ys)+1))
+    ax1.clear()
+    ax1.plot(xs,ys)
+    ax1.set_title('TESLA', fontsize =12)
+    
+    ys = df.iloc[1:,3].values
+    ax2.clear()
+    ax2.plot(xs,ys)
+    ax2.set_title('APPLE', fontsize =12)
+    
+    ys = df.iloc[1:,4].values
+    ax3.clear()
+    ax3.plot(xs,ys)
+    ax3.set_title('BIONTECH', fontsize =12)
+    
+    ys = df.iloc[1:,5].values
+    ax4.clear()
+    ax4.plot(xs,ys)    
+    ax4.set_title('THE COCA-COLA COMPANY', fontsize =12)
+    
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+
+plt.tight_layout()
+plt.show()
+    
