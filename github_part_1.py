db094a03ef849c7a614999712b42ecc574b623ea
Arturo9561 <37811770+Arturo9561@users.noreply.github.com>
2021-02-20 01:03:22 +0100

    BestInvest.py (Created as a portfolio optimization application via using the efficient frontiers algorithm
    BestInvest.ipynb is the Notebook version of the file BestInvest.py, this is suggested to be used.
    
    -Automatic allocation of a given investment.

--- github_part_1.py
+++ github_part_1.py
@@ -16,31 +16,38 @@ def real_time_price(symbol):
     r = requests.get(url)
     web_content = BeautifulSoup(r.text, 'lxml')
     web_content = web_content.find('div', {"class":'My(6px) Pos(r) smartphone_Mt(6px)'})
-    web_content = web_content.find('span').text
+    web_content = web_content.find_all('span', {"class":'Trsdu(0.3s) Fw(500) Pstart(10px) Fz(24px) C($positiveColor)'   })
+#    current_value = web_content[0].text
 
+#    print(current_value)
+    
+    for x in range(len(web_content)):
+        print(web_content[x].text)
     return web_content
 
-NASDAQ = ['TSLA', 'AAPL', 'BNTX', 'KO']
-web_content = real_time_price('TSLA')
+real_time_price('TSLA')
 
-for step in range(1,101):   
+# NASDAQ = ['TSLA', 'AAPL', 'BNTX', 'KO']
+
+
+# for step in range(1,101):   
     
-    price = []
-    col = []
-    time_stamp = dt.datetime.now()
-    time_stamp = time_stamp.strftime("%Y-%m-%d %H:%M:%S")
+#     price = []
+#     col = []
+#     time_stamp = dt.datetime.now()
+#     time_stamp = time_stamp.strftime("%Y-%m-%d %H:%M:%S")
     
-    for symbol in NASDAQ:
-        price.append(real_time_price(symbol))
+#     for symbol in NASDAQ:
+#         price.append(real_time_price(symbol))
         
-    col = [time_stamp]
-    col.extend(price)
+#     col = [time_stamp]
+#     col.extend(price)
     
-    df = pd.DataFrame(col)
+#     df = pd.DataFrame(col)
     
-    df = df.T
+#     df = df.T
     
-    df.to_csv('real time stock data.csv', mode = 'a', header = False)
-    print(col)
+#     df.to_csv('real time stock data.csv', mode = 'a', header = False)
+#     print(col)
     
     
